fcb702ba9c4fd1b038a924edbfb39275d1231d83
behnam farhadi <farhadi.behnam@gmail.com>
2025-07-03 15:31:41 -0230

    Final Solution

--- 1.js
+++ 1.js
@@ -1,10 +1,7 @@
 const calcAverage  = (a,b,c) => (a+b+c)/3;
 
-
-const avgDolphins1 = (calcAverage (44,23,71));
-const avgKoalas1 =  (calcAverage (65,454,49));
-//const avgDolphins2 = (calcAverage (85, 54, 41));
-//const avgKoalas2 =  (calcAverage (23, 34, 27));
+let  avgDolphins1 = (calcAverage (44,23,71));
+let  avgKoalas1 =  (calcAverage (65,454,49));
 
 console.log (avgDolphins1,avgKoalas1)
 
@@ -20,3 +17,10 @@ console.log ('No Team Wins');
 } 
 };
 checkWinner (avgDolphins1, avgKoalas1)
+
+avgDolphins1 = (calcAverage (85, 54, 41));
+avgKoalas1 =  (calcAverage (23, 34, 27));
+
+console.log (avgDolphins1, avgKoalas1);
+checkWinner (avgDolphins1, avgKoalas1);
+
